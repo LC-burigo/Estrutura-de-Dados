6ab9f2f8b37cf2b4838e0124b2ef14d4a27bc1fc
LC-burigo <burigo-jm@hotmail.com>
2020-12-29 14:37:12 -0300

    linkedlist

--- Lista_Encadeada.py
+++ Lista_Encadeada.py
@@ -9,15 +9,18 @@ class LinkedList:
         self.head = None
 
     def append(self, data):
+        new_node = Node(data)
         # se não há nenhum nó, adicionar esta primeira data ao primeiro nó, que estará armazenado dentro do self.head
         if self.head is None:
-            self.head = Node(data)
+            self.head = new_node
+            return
 
         current_node = self.head
 
         while current_node.next:
             current_node = current_node.next
-        current_node = Node(data)
+        current_node = new_node
+        return
 
     def lenght(self):
         node_size = 0
@@ -43,6 +46,46 @@ class LinkedList:
             current_node = current_node.next
         return nodes_list
 
+    def get_by_index(self, index):
+        current_node = self.head
+        current_index = 0
+
+        if index < 0 or index > len(self.list()):
+            print("ERROR: 'Get' Index out of range!")
+            return None
+
+        if current_node is None:
+            print("ERROR: 'Get' Index out of range!")
+
+        while current_node:
+            if current_index == index:
+                return current_node.data
+            current_node = current_node.next
+            current_index += 1
+
+    def get_by_value(self, value):
+        current_node = self.head
+
+        if current_node is None:
+            print("List has no elements")
+
+        while current_node:
+            if current_node.data == value:
+                print("Item found")
+                return True
+            current_node = current_node.next
+        print("Item not found")
+        return False
+
+
+one = LinkedList()
+print(one.list())
+one.append(4)
+one.append(17)
+one.append(2)
+one.append(1)
+one.append(24)
+print(one.list())
 
 
 
