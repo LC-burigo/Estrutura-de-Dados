0ab31fda8608fe07e8bef45cd1098bfb06585dd6
LC-burigo <burigo-jm@hotmail.com>
2020-12-31 19:47:36 -0300

    testes

---
+++ teste.py
@@ -0,0 +1,34 @@
+# def two_in_two(arr):
+#     two_element = 0
+#     max_sum = arr[0] + arr[1]
+#
+#     i = 1
+#     while i < len(arr) - 1:
+#         two_element = arr[i] + arr[i + 1]
+#         if two_element > max_sum:
+#             max_sum = two_element
+#         i += 1
+#     return max_sum
+#
+#
+# arr = [1, 2, 20, -10, 40]
+# print(two_in_two(arr))
+#
+#
+# def three_in_three(arr):
+#     three_element = 0
+#     max_sum = arr[0] + arr[1] + arr[2]
+#
+#     i = 1
+#     while i < len(arr) - 2:
+#         three_element = arr[i] + arr[i + 1] + arr[i + 2]
+#         if three_element > max_sum:
+#             max_sum = three_element
+#         i += 1
+#     return max_sum
+#
+#
+# arr = [1, 2, 20, 10, 40]
+# print(three_in_three(arr))
+
+
